515b24f4dd2bb2a772aa1b5486d452cdde5858bc
Nandi Samuel <nandisamuelsne@gmail.com>
2025-06-14 13:14:22 +0100

    add an announcement components

---
+++ src/app/_components/announcements.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import { ArrowRightIcon } from "lucide-react"
+import { Badge } from "~/components/ui/badge"
+
+
+export function Announcement() {
+  return (
+    <Badge asChild variant="secondary" className="rounded-full">
+      <Link href="/docs/components/calendar">
+        Nouvelle collection <ArrowRightIcon />
+      </Link>
+    </Badge>
+  )
+}
+
