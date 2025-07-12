d4a73f0bab325a27b88241e32d922364c3fa1161
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-12 10:36:50 +0100

    feat: add the layout for all the pages
    This allow to get certain element such as `headers` and `footers`
    on all page without having to manually add them one by one.

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "../_components/header"
+import { Footer } from "../_components/footer";
+
+interface AppLayoutProps {
+  children: React.ReactNode
+}
+
+export default function AppLayout({ children }: AppLayoutProps) {
+  return (
+    <div data-wrapper="" className="border-grid flex flex-1 flex-col">
+      <Header />
+      <main className="flex flex-1 flex-col">{children}</main>
+      <Footer />
+    </div>
+  )
+}
