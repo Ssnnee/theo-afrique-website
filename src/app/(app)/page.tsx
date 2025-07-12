d8ba5dffdb11b30164a4b2a6d7a4dcfb164a98bd
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-12 10:41:40 +0100

    feat: add a limited product list to the home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 
 import { HydrateClient, api } from "~/trpc/server";
-import { Header } from "../_components/header";
 import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from "../_components/page-header";
 import { Button } from "~/components/ui/button";
-import { Footer } from "../_components/footer";
 import { About } from "../_components/about";
-import { ProductNav } from "../_components/category-nav";
+import { CategoriesNav } from "../_components/category-nav";
 import { Announcement } from "../_components/announcements";
+import { LimitedProducts } from "../_components/limited-products-list";
 
 export default async function Home() {
 
@@ -17,14 +16,10 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="">
-        <Header />
         <PageHeader >
-        <Announcement />
           <PageHeaderHeading>
             Bienvenue chez Theo Afrique
             <span className="text-primary">.</span>
-            <br />
-            Où la tradition rencontre l'audace.
           </PageHeaderHeading>
           <PageHeaderDescription>
             Découvrez l'harmonie parfaite entre l'artisanat traditionnel africain
@@ -39,15 +34,15 @@ export default async function Home() {
           </PageActions>
         </PageHeader>
         <div className="container py-4">
-        <ProductNav />
+          <CategoriesNav />
         </div>
+        <LimitedProducts limit={8} />
         <section className="p-4">
         </section>
         <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
           <h1 className="text-2xl font-bold mb-4"> À propos de nous</h1>
           <About />
         </section>
-        <Footer />
       </main>
     </HydrateClient>
   );
