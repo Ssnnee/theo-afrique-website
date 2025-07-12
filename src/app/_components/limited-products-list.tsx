5b0c168d95ed758762ba4edc34d208468c2c8cf1
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-12 10:39:56 +0100

    feat: add a ameliorate loading state and a not found state

--- src/app/_components/limited-products-list.tsx
+++ src/app/_components/limited-products-list.tsx
@@ -4,12 +4,18 @@ import { ProductList } from "./product-list";
 
 export function LimitedProducts( { limit }: { limit: number }) {
   const { data: productList = [], isLoading } = api.product.getLimited.useQuery({ limit });
-  console.log("Here the limited list", productList)
 
   return (
-    <div>
-      {isLoading && <p>Loading...</p>}
-      <ProductList products={productList} />
+    <div className="flex items-center justify-center">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) :
+        productList.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+            <ProductList products={productList} />
+          )
+      }
     </div>
   );
 }
