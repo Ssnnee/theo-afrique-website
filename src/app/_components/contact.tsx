843a866385a58f90e4dbe5ad3c3d61cd8f2de0dc
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-14 13:54:15 +0100

    feat: make the contact forms

--- src/app/_components/contact.tsx
+++ src/app/_components/contact.tsx
@@ -1,25 +1,93 @@
-export default function Contact() {
+"use client"
+
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+import { ContactSchema } from "~/types";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { Textarea } from "~/components/ui/textarea";
+
+export default function ContactForm() {
+  const form = useForm<z.infer<typeof ContactSchema>>({
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+      subject: "",
+    }
+  })
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+
+  }
+
   return (
-    <div className="container mx-auto px-4 py-12">
-      <h1 className="text-3xl font-bold mb-6">Contactez-nous</h1>
-      <p className="mb-4">
-        Si vous avez des questions, des commentaires ou des préoccupations, n'hésitez pas à nous contacter. Nous sommes là pour vous aider !
-      </p>
-      <form className="space-y-6">
-        <div>
-          <label htmlFor="name" className="block text-sm font-medium text-gray-700">Nom</label>
-          <input type="text" id="name" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required />
-        </div>
-        <div>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-          <input type="email" id="email" className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required />
-        </div>
-        <div>
-          <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
-          <textarea id="message" rows={4} className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500" required></textarea>
-        </div>
-        <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">Envoyer</button>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 text-xl">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input {...field} type="text" id="name" placeholder="Nom et Prénom" />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+        />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input type="email" id="email" placeholder="Email" {...field} />
+            </FormControl>
+            <FormDescription>
+              Cette adresse email sera utilisée pour répondre à votre message
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+        />
+      <FormField
+        control={form.control}
+        name="subject"
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input type="text" id="subject" placeholder="Sujet" {...field} />
+            </FormControl>
+            <FormDescription>
+              Veuillez renseigner la raison pour laquelle vous souhaitez nous contacter
+            </FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tapez votre message ici"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Entrez votre message
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Envoyer </Button>
       </form>
-    </div>
-  );
+    </Form>
+  )
 }
