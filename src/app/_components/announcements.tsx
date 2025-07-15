461e6c65dd0ba0a90d4a52c16894591d86e4e00f
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-15 16:43:15 +0100

    feat: put the right link path

--- src/app/_components/announcements.tsx
+++ src/app/_components/announcements.tsx
@@ -6,7 +6,7 @@ import { Badge } from "~/components/ui/badge"
 export function Announcement() {
   return (
     <Badge asChild variant="secondary" className="rounded-full">
-      <Link href="/docs/components/calendar">
+      <Link href="/collection">
         Nouvelle collection <ArrowRightIcon />
       </Link>
     </Badge>
