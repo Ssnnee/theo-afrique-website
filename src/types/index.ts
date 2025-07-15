db34fb417bb4f50e13b56f41e8a4e8a6a0a36fe5
Nandi Samuel <nandisamuelsne@gmail.com>
2025-07-15 13:02:00 +0100

    feat: add schema for order form

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,5 @@
 import { z } from 'zod';
 
-
-// type Product = {
-//   id: number;
-//   name: string;
-//   description: string;
-//   price: number;
-//   imageUrl: string;
-//   sizes: string[];
-//   colors: string[];
-//   stock: number;
-//   createdAt: Date;
-//   updatedAt: Date | null;
-// };
-
 export const ProductSchema = z.object({
   id: z.number(),
   name: z.string().min(2, { message: "Le nom doit comporter au moins 2 caractères" }),
@@ -28,3 +14,37 @@ export const ProductSchema = z.object({
   createdAt: z.date(),
   updatedAt: z.date().nullable(),
 });
+
+export const ContactSchema = z.object({
+  name: z.string().min(5, { message: "Le champs doit contenir au moins 5 caractères" }),
+  email: z.string().email({ message: "Veullez entrer une adresse email valide" }),
+  subject: z.string().min(5, { message: "Le sujet doit contenir au moins 5 caractères" }),
+  message: z
+    .string()
+    .min(160, {
+      message: "Le message doit contenir au moins 160 caractères",
+    })
+    .max(500, {
+      message: "Le message ne doit pas depasser 500 caractères",
+    })
+})
+
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
+
+export const OrderFormSchema = z.object({
+  name: z.string().min(2, { message: "Le nom est requis" }),
+  phone: z.string().regex(phoneRegex, {
+    message: "Numéro de téléphone invalide"
+  }),
+  quantity: z
+  .number({ invalid_type_error: "Quantité invalide" })
+  .min(1, { message: "La quantité doit être d'au moins 1." }),
+  selectedSizes: z.array(z.string()),
+  selectedColors: z.array(z.string()),
+  customSizing: z.boolean().optional(),
+  shirtSize: z.string().optional(),
+  pantsSize: z.string().optional(),
+});
+
